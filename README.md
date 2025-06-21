d171c1458e80f2cec765a56e30cdf5316e8c8ab6
Muhammad Musa Khan <musa.socials@gmail.com>
2025-06-21 18:01:20 +0400

    add linkedlistdemo.java, clean.sh, README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+To run, first clean: ./clean.sh
+Then, run infer: infer run --purity -- javac LinkedListDemo.java
---
+++ clean.sh
@@ -0,0 +1,4 @@
+rm -r infer-out
+rm Node.class
+rm LinkedList*.class
+echo "cleaned infer-out, LinkedListDemo*.class"
